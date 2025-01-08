824f4ba7b04b59daff7ba2afed4a4edac6bd9b9c
Junjak <haring157@naver.com>
2025-01-08 17:14:33 +0900

    eslint 추가 및 테스트

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  astro: true,
+  react: true,
+  typescript: true,
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+    semi: true,
+  },
+  formatters: {
+    /**
+     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+     * By default uses Prettier
+     */
+    css: true,
+    /**
+     * Format HTML files
+     * By default uses Prettier
+     */
+    html: true,
+    /**
+     * Format Markdown files
+     * Supports Prettier and dprint
+     * By default uses Prettier
+     */
+    markdown: 'prettier',
+  },
+}, {
+  rules: {
+    'style/arrow-parens': ['error', 'always'], // arrow function 괄호 사용 설정
+    'node/prefer-global/process': 'off', // process.env 사용을 위해 off
+    'eslint-disable-next-line': 'off', // eslint-disable 사용을 위해 off
+    'ts/no-namespace': 'off',
+    'ts/no-use-before-define': 'off',
+    'ts/ban-ts-comment': 'off',
+    'no-unused-vars': 'warn',
+    'no-underscore-dangle': 'off',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'ts/consistent-type-definitions': ['off'],
+    'unused-imports/no-unused-vars': ['warn'],
+    'unused-imports/no-unused-imports': ['warn'],
+  },
+});
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <nav>
+        <a href="/">Home</a>
+        |
+        <a href="/about">About</a>
+        |
+        <a href="/blog">Blog</a>
+      </nav>
+    </nav>
+  );
+}
