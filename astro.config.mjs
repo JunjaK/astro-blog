f3e9b56515eb66fe0e42c5d14728f99fd3143f4d
yoonjunhyeon <yoonjunhyeon@twentyoz.kr>
2025-02-11 09:26:04 +0900

    색 변수 추가 및 미사용 라이브러리 제거

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,6 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
-import rehypeFormat from 'rehype-format';
-import rehypeRaw from 'rehype-raw';
-import rehypeSanitize from 'rehype-sanitize';
-import remarkDirective from 'remark-directive';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math';
-import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,20 +16,6 @@ export default defineConfig({
     shikiConfig: {
       theme: 'github-dark-dimmed',
     },
-    remarkPlugins: [
-      [remarkToc, { heading: 'toc', maxDepth: 3 }],
-      [remarkDirective],
-      [remarkFrontmatter],
-      [remarkGfm],
-      [remarkMath],
-    ],
-    rehypePlugins: [
-      [rehypeRaw],
-      [rehypeFormat],
-      [rehypeSanitize],
-      [rehypeAccessibleEmojis],
-
-    ],
   },
   vite: {
     ssr: {
