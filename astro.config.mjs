a6b9ab3825f578711f2fde4c11698ce6895b7b78
yoonjunhyeon <yoonjunhyeon@twentyoz.kr>
2025-02-13 05:10:00 +0900

    빌드 오류 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from '@astrojs/tailwind';
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
 
-import viteImagemin from 'vite-plugin-imagemin';
-
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
@@ -30,33 +28,7 @@ export default defineConfig({
       include: ['react-use'],
     },
     plugins: [
-      viteImagemin({
-        gifsicle: {
-          optimizationLevel: 7,
-          interlaced: false,
-        },
-        optipng: {
-          optimizationLevel: 7,
-        },
-        mozjpeg: {
-          quality: 20,
-        },
-        pngquant: {
-          quality: [0.1, 0.2],
-          speed: 4,
-        },
-        svgo: {
-          plugins: [
-            {
-              name: 'removeViewBox',
-            },
-            {
-              name: 'removeEmptyAttrs',
-              active: false,
-            },
-          ],
-        },
-      }),
+
     ],
   },
 
