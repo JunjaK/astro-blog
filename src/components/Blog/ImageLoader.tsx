ac5aab076350098baf2fa5fc600eab7c1831c3ac
Junjak <haring157@gmail.com>
2025-02-27 21:54:29 +0900

    imageLoader, toc 조정

--- src/components/Blog/ImageLoader.tsx
+++ src/components/Blog/ImageLoader.tsx
@@ -1,18 +1,23 @@
+import type { SyntheticEvent } from 'react';
 import { getBasePathWithUrl } from '@/utils/getBasePathWithUrl.ts';
 import * as React from 'react';
 
 type ImageLoaderProps = {
-  src: string;
+  src?: string;
   alt?: string;
 } & React.ImgHTMLAttributes<HTMLImageElement>;
 
 export default function ImageLoader({ src, alt = 'blog-image', ...props }: ImageLoaderProps) {
+  const addDefaultImg = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = '/fallbackImg.svg';
+  };
   return (
     <img
       {...props}
       src={getBasePathWithUrl(src)}
       alt={alt}
       loading="lazy"
+      onError={addDefaultImg}
     />
   );
 }
