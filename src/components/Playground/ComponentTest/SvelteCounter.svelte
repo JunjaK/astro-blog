18cdcf9c71194f7900252a73b5a666ca62c7cd56
Junjak <haring157@naver.com>
2025-03-06 20:04:46 +0900

    playground 페이지 추가

---
+++ src/components/Playground/ComponentTest/SvelteCounter.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Icon } from "@iconify/react";
+
+  let counter = $state(0);
+  function counterChange(arg: "increment" | "decrement") {
+    if (arg === "increment") {
+      counter++;
+    } else {
+      counter--;
+    }
+  }
+
+  const doubleCount = $derived(counter * 2);
+</script>
+
+<div class="flex items-center justify-between mx-auto w-1/2">
+  <button class="text-2xl" onclick={() => counterChange("decrement")}>
+    -
+  </button>
+  <p class="text-2xl font-bold">
+    {counter}
+  </p>
+  <button class="text-2xl" onclick={() => counterChange("increment")}>
+    +
+  </button>
+</div>
+
+<div>
+  DoubleCount: {doubleCount}
+</div>
---
+++ src/components/Playground/ComponentTest/VueCounter.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import { computed, ref } from 'vue';
+
+const count = ref(0);
+const doubleCount = computed(() => count.value * 2);
+
+function counterChange(arg: 'increment' | 'decrement') {
+  if (arg === 'increment') {
+    count.value++;
+  }
+  else {
+    count.value--;
+  }
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-between mx-auto w-1/2">
+    <button class="text-2xl" @click="counterChange('decrement')">
+      <Icon icon="tabler:minus" />
+    </button>
+    <p class="text-2xl font-bold">
+      {{ count }}
+    </p>
+    <button class="text-2xl" @click="counterChange('increment')">
+      <Icon icon="tabler:plus" />
+    </button>
+  </div>
+
+  <div>
+    DoubleCount: {{ doubleCount }}
+  </div>
+</template>
